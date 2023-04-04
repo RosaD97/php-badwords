7c7af51ab171e1b937f1a162075111c8de6f1b75
RosaD97 <r960595@gmail.com>
2023-04-04 18:20:33 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
-<?php 
-
-$name = "Paolo";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>ciao sono <?php echo $name ?> </h1>
+
+    <form action="main.php" method="GET">
+        <input name="testo" type="text" class="form-control" placeholder="Scrivi la frase">
+        <input name="censura" type="text" class="form-control" placeholder="La parola da censurare">
+        <button class="btn btn-outline-secondary">Invia</button>
+    </form>
 </body>
+
 </html>
